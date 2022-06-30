4ae56f87d9a57a420fe68d8fe63f7ecd68eaaebe
pooya98 <pooya98@naver.com>
2022-06-30 14:06:38 +0900

    MainActivity 하단 네비게이션바 선언, 네비게이션 각 버튼 클릭시 해당 프래그먼트로 전환

--- app/src/main/java/com/example/values/Activity/MainActivity.kt
+++ app/src/main/java/com/example/values/Activity/MainActivity.kt
@@ -13,5 +13,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // <------ 레이아웃 뷰 선언 ------> //
+
+        // 하단 네비게이션 바
+        val bottomMenu = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+
+        // <------ 뷰 이벤트 리스너 선언 ------> //
+
+        // 하단 네비게이션 바 클릭 리스너
+        bottomMenu.run { setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    val homeFragment = Fragment_01()
+                    supportFragmentManager.beginTransaction().replace(R.id.Main_FrameLayout, Fragment_01()).commit()
+                }
+                R.id.display -> {
+                    val homeFragment = Fragment_02()
+                    supportFragmentManager.beginTransaction().replace(R.id.Main_FrameLayout, Fragment_02()).commit()
+                }
+                R.id.ticketing -> {
+                    val homeFragment = Fragment_03()
+                    supportFragmentManager.beginTransaction().replace(R.id.Main_FrameLayout, Fragment_03()).commit()
+                }
+                R.id.menu -> {
+                    val homeFragment = Fragment_04()
+                    supportFragmentManager.beginTransaction().replace(R.id.Main_FrameLayout, Fragment_04()).commit()
+                }
+            }
+            true
+        } }
+
     }
 }
