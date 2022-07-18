30f5d119b09e3d452b154b94fbf7ce6602f2f681
pooya98 <pooya98@naver.com>
2022-07-18 17:46:25 +0900

    Fragment03 수정

--- app/src/main/java/com/example/values/Fragment/Fragment_03.kt
+++ app/src/main/java/com/example/values/Fragment/Fragment_03.kt
@@ -6,55 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.values.R
+import com.example.values.databinding.Fragment03Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment_03.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Fragment_03 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var mBinding : Fragment03Binding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_03, container, false)
+        val binding = Fragment03Binding.inflate(inflater, container,false)
+
+        mBinding = binding
+
+        return mBinding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_03.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment_03().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        mBinding = null   //메모리에서 다시 날려주기위함
+        super.onDestroyView()
     }
+
+
 }
