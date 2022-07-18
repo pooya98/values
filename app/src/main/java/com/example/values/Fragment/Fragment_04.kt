8a6b371cd1d34be86355a258d56044a184d1e1f5
pooya98 <pooya98@naver.com>
2022-07-18 17:47:02 +0900

    Fragment04 수정

--- app/src/main/java/com/example/values/Fragment/Fragment_04.kt
+++ app/src/main/java/com/example/values/Fragment/Fragment_04.kt
@@ -1,60 +1,53 @@
 package com.example.values.Fragment
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.values.R
+import com.example.values.databinding.Fragment04Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment_04.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Fragment_04 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Fragment_04 : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var mBinding : Fragment04Binding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_04, container, false)
+        val binding = Fragment04Binding.inflate(inflater, container,false)
+        mBinding = binding
+        mBinding?.myValuesButton?.setOnClickListener(this)
+        mBinding?.eventButton?.setOnClickListener(this)
+
+        return mBinding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_04.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment_04().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        mBinding = null   //메모리에서 다시 날려주기위함
+        super.onDestroyView()
     }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+            R.id.myValuesButton -> {
+                findNavController().navigate(R.id.action_fragment_04_to_fragment_04_01)
+            }
+            R.id.eventButton -> {
+                findNavController().navigate(R.id.action_fragment_04_to_fragment_04_02)
+            }
+        }
+    }
+
+
+
 }
