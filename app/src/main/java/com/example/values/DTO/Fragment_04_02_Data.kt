fc173dff3afaf5cdd16c7e6e2e44e41de731869f
pooya98 <pooya98@naver.com>
2022-07-18 17:41:07 +0900

    DTO 생성 (1)

---
+++ app/src/main/java/com/example/values/DTO/Fragment_04_02_Data.kt
@@ -0,0 +1,9 @@
+package com.example.values.DTO
+
+  data class Fragment_04_02_Data (
+    val eventTitle : String,
+    val period : String,
+    val views : String,
+    val comments : String,
+
+    )
