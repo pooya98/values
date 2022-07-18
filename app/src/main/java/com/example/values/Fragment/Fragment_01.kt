26e24f6852a362b41df0552aaabfd74973205e5f
pooya98 <pooya98@naver.com>
2022-07-18 17:45:02 +0900

    Fragment01 수정

--- app/src/main/java/com/example/values/Fragment/Fragment_01.kt
+++ app/src/main/java/com/example/values/Fragment/Fragment_01.kt
@@ -6,82 +6,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.values.R
+import com.example.values.databinding.Fragment01Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class Fragment_01 : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Fragment_01.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Fragment_01 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var mBinding : Fragment01Binding? = null
+    lateinit var navController : NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val binding = Fragment01Binding.inflate(inflater, container, false)
 
-        val view = inflater.inflate(R.layout.fragment_01, container, false)
+        mBinding = binding
 
-        val topMenuButtons = view.findViewById<RadioGroup>(R.id.fragment01_radioGroup)
+        var navHostFragment = childFragmentManager.findFragmentById(R.id.fragment01_FrameLayout) as NavHostFragment
+        navController = navHostFragment.navController
 
-        topMenuButtons.setOnCheckedChangeListener{ radioGroup, checkedId ->
-            when(checkedId){
-                R.id.fragment01_radio1 -> {
-                    childFragmentManager.beginTransaction().replace(R.id.fragment01_FrameLayout, Fragment_01_01()).commit()
-                }
+        mBinding?.fragment01Radio1?.setOnClickListener(this)
+        mBinding?.fragment01Radio2?.setOnClickListener(this)
+        mBinding?.fragment01Radio3?.setOnClickListener(this)
+        mBinding?.fragment01Radio4?.setOnClickListener(this)
 
-                R.id.fragment01_radio2 -> {
-
-                }
-
-                R.id.fragment01_radio3 -> {
-
-                }
-
-                R.id.fragment01_radio4 -> {
-
-                }
-            }
-        }
-
-        childFragmentManager.beginTransaction().replace(R.id.fragment01_FrameLayout, Fragment_01_01()).commit()
         // Inflate the layout for this fragment
-        return view
+        return mBinding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_01.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Fragment_01().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onDestroyView() {
+        mBinding = null   //메모리에서 다시 날려주기위함
+        super.onDestroyView()
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+            R.id.fragment01_radio1 -> {
+                navController.navigate(R.id.fragment_01_01)
+
             }
+            R.id.fragment01_radio2 -> {
+                navController.navigate(R.id.fragment_01_02)
+            }
+            R.id.fragment01_radio3 -> {
+                navController.navigate(R.id.fragment_01_03)
+            }
+            R.id.fragment01_radio4 -> {
+                navController.navigate(R.id.fragment_01_04)
+            }
+
+        }
     }
 }
