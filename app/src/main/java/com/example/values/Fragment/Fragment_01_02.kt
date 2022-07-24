356ad1c12d304bd81e2e54066c42005088e0ef7c
pooya98 <pooya98@naver.com>
2022-07-24 17:29:30 +0900

    Fragment01_02 ViewPager 추가

--- app/src/main/java/com/example/values/Fragment/Fragment_01_02.kt
+++ app/src/main/java/com/example/values/Fragment/Fragment_01_02.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.values.Adapter.Fragment_01_01_branding_Adapter
+import com.example.values.Adapter.Fragment_01_01_illustration_Adapter
+import com.example.values.Adapter.Fragment_01_02_branding_Adapter
+import com.example.values.Adapter.Fragment_01_02_illustration_Adapter
 import com.example.values.R
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator
 
 class Fragment_01_02 : Fragment() {
 
@@ -14,6 +20,58 @@ class Fragment_01_02 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_01_02, container, false)
+        val view = inflater.inflate(R.layout.fragment_01_02, container, false)
+        // Inflate the layout for this fragment
+
+        val branding_viewpager = view.findViewById<ViewPager2>(R.id.fragment01_02_branding_viewpager)
+        val illustration_viewpager = view.findViewById<ViewPager2>(R.id.fragment01_02_illustration_viewpager)
+
+        val branding_indicator = view.findViewById<SpringDotsIndicator>(R.id.fragment01_02_branding_indicator)
+        val illustration_indicator = view.findViewById<SpringDotsIndicator>(R.id.fragment01_02_illustration_indicator)
+
+
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // dimen 파일 안에 크기를 정의해두었다.
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth) // dimen 파일이 없으면 생성해야함
+        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
+
+        branding_viewpager.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+        }
+
+        branding_viewpager.offscreenPageLimit = 2
+
+        branding_viewpager.adapter = Fragment_01_02_branding_Adapter(arrayListOf<Int>(
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background))
+
+
+        branding_viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        branding_viewpager.setCurrentItem(2)
+
+        branding_indicator.attachTo(branding_viewpager)
+
+        illustration_viewpager.setPageTransformer { page, position ->
+            page.translationX = position * -offsetPx
+        }
+
+        illustration_viewpager.offscreenPageLimit = 2
+
+        illustration_viewpager.adapter = Fragment_01_02_illustration_Adapter(arrayListOf<Int>(
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background))
+
+        illustration_viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        illustration_viewpager.setCurrentItem(2)
+
+        illustration_indicator.attachTo(illustration_viewpager)
+
+        return view
     }
 }
