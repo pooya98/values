6620309a6c89d5707dc7ef420fc1aeff618bcad4
pooya98 <pooya98@naver.com>
2022-07-28 19:12:20 +0900

    fragment_01_01 spinner 추가

--- app/src/main/java/com/example/values/Fragment/Fragment_01_01.kt
+++ app/src/main/java/com/example/values/Fragment/Fragment_01_01.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.viewpager2.widget.ViewPager2
 import com.example.values.Adapter.Fragment_01_01_branding_Adapter
 import com.example.values.Adapter.Fragment_01_01_illustration_Adapter
@@ -22,11 +24,24 @@ class Fragment_01_01 : Fragment() {
 
         val branding_viewpager = view.findViewById<ViewPager2>(R.id.fragment01_01_branding_viewpager)
         val illustration_viewpager = view.findViewById<ViewPager2>(R.id.fragment01_01_illustration_viewpager)
-
         val branding_indicator = view.findViewById<SpringDotsIndicator>(R.id.fragment01_01_branding_indicator)
         val illustration_indicator = view.findViewById<SpringDotsIndicator>(R.id.fragment01_01_illustration_indicator)
+        val main_spinner = view.findViewById<Spinner>(R.id.fragment_01_01_main_spinner)
 
 
+        // viewPager 설정
+        set_branding_viewPager(branding_viewpager)
+        set_branding_viewPager_indicator(branding_viewpager, branding_indicator)
+        set_illustration_viewPager(illustration_viewpager)
+        set_illustration_viewPager_indicator(illustration_viewpager, illustration_indicator)
+
+        set_main_spinner(main_spinner)
+
+        return view
+    }
+
+
+    private fun set_branding_viewPager(branding_viewpager: ViewPager2){
         val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // dimen 파일 안에 크기를 정의해두었다.
         val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth) // dimen 파일이 없으면 생성해야함
         val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
@@ -48,8 +63,17 @@ class Fragment_01_01 : Fragment() {
 
         branding_viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         branding_viewpager.setCurrentItem(2)
+    }
 
+    private fun set_branding_viewPager_indicator(branding_viewpager: ViewPager2, branding_indicator: SpringDotsIndicator){
         branding_indicator.attachTo(branding_viewpager)
+    }
+
+    private fun set_illustration_viewPager(illustration_viewpager: ViewPager2){
+        val pageMarginPx = resources.getDimensionPixelOffset(R.dimen.pageMargin) // dimen 파일 안에 크기를 정의해두었다.
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pageWidth) // dimen 파일이 없으면 생성해야함
+        val screenWidth = resources.displayMetrics.widthPixels // 스마트폰의 너비 길이를 가져옴
+        val offsetPx = screenWidth - pageMarginPx - pagerWidth
 
         illustration_viewpager.setPageTransformer { page, position ->
             page.translationX = position * -offsetPx
@@ -57,18 +81,32 @@ class Fragment_01_01 : Fragment() {
 
         illustration_viewpager.offscreenPageLimit = 2
 
-        illustration_viewpager.adapter = Fragment_01_01_illustration_Adapter(arrayListOf<Int>(
+        illustration_viewpager.adapter = Fragment_01_01_branding_Adapter(arrayListOf<Int>(
             R.drawable.ic_launcher_background,
             R.drawable.ic_launcher_background,
             R.drawable.ic_launcher_background,
             R.drawable.ic_launcher_background,
             R.drawable.ic_launcher_background))
 
+
         illustration_viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         illustration_viewpager.setCurrentItem(2)
+    }
 
+    private fun set_illustration_viewPager_indicator(illustration_viewpager: ViewPager2, illustration_indicator: SpringDotsIndicator){
         illustration_indicator.attachTo(illustration_viewpager)
+    }
 
-        return view
+    private fun set_main_spinner(main_spinner: Spinner){
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.spinner_list_fragment_01_01,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            main_spinner.adapter = adapter
+        }
     }
 }
